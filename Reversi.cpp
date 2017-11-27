b66dca952bc4300e1290f72fd6d8eb75495896bd
aradzu10 <aradzulti@gmail.com>
2017-11-27 21:28:20 +0200

    test1

--- Reversi.cpp
+++ Reversi.cpp
@@ -3,13 +3,11 @@ Name: Arad Zulti
 ID: 315240564
 */
 
+#include <iostream>
+using namespace std;
 #include "Reversi.h"
-#include "GameManager.h"
-#include "ConsolePlayer.h"
 #include "AIPlayer.h"
-#include "BoardManager.h"
 #include "ConsolePrinter.h"
-#include "RegularRules.h"
 #include "Graphic.h"
 
 Reversi::Reversi(int size) : board_size(size), board(board_size), game(board_size, board) {
@@ -20,9 +18,16 @@ Reversi::~Reversi() {
 }
 
 void Reversi::ChangeSettings() {
-	Graphic* printer = new ConsolePrinter();
-	game.SetPrinter(printer);
-	game.SetPlayers(new AIPlayer(White, board, printer), new AIPlayer(Black, board, printer));
+	int task;
+	cout << "Please choose type of opponent" << endl;
+	cout << "1 - AI player" << endl;
+	cout << "2 - Human player" << endl;
+	cin >> task;
+	if (task == 1) {
+		Graphic *printer = new ConsolePrinter();
+		game.SetPrinter(printer);
+		game.SetPlayers(new AIPlayer(White, board, printer), new AIPlayer(Black, board, printer));
+	}
 }
 
 void Reversi::StartGame() {
--- ex3.cpp
+++ ex3.cpp
@@ -6,7 +6,6 @@ ID: 315240564
 #include "Reversi.h"
 
 int main() {
-
 	Reversi reversi(8);
 	reversi.StartGame();
 	return 0;
