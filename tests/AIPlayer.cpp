6030d204b8567adf99748e7610d169bb15df4cd0
aradzu10 <aradzulti@gmail.com>
2017-11-28 22:30:14 +0200

    AI player Test

---
+++ tests/AIPlayer.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../src/AIPlayer.h"
+#include "../src/Checker.h"
+#include "../src/BoardManager.h"
+#include "../src/ConsolePrinter.h"
+
+TEST(AI_PLAYER_TEST, GET_POINT) {
+    BoardManager boardManager(4);
+    Matrix<Checker> board(4, 4);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            board[i][j] = Nothing;
+        }
+    }
+    board[0][0] = White;
+    board[0][1] = White;
+    board[1][0] = Black;
+    board[2][0] = AvailableW;
+    boardManager.Init(board);
+    Graphic* printer = new ConsolePrinter();
+    AIPlayer playerW(White, boardManager, printer);
+    AIPlayer playerB(Black, boardManager, printer);
+    Point point1 = playerW.GetPointFromPlayer();
+    Point point2 = playerB.GetPointFromPlayer();
+    Point tmp1(2, 0);
+    Point tmp2(-1, -1);
+    EXPECT_TRUE(tmp1 == point1 && tmp2 == point2);
+}
+
